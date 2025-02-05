9aa8a75d4e93c0069ad960b1db787d82c6bbb3df
AQuan05 <trandihanhquan17072021@gmail.com>
2025-02-05 17:41:42 +0700

    soat loi

--- index.php
+++ index.php
@@ -70,10 +70,10 @@ if (isset($_GET['act']) && $_GET['act'] != '') {
         case 'Faq':
             include 'view/pagines/pages/Faq.php';
             break;
-        case 'detailProducts':
-            $productController = new ProductController();
-            $productController->detailProductsController($_GET['product_id']);
-            break;
+        // case 'detailProducts':
+        //     $productController = new ProductController();
+        //     $productController->detailProductsController($_GET['product_id']);
+        //     break;
     }
 } else {
 
