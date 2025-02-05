f5e287e12db8a4bff728a5f2ea2a42391a5dd0ec
AQuan05 <trandihanhquan17072021@gmail.com>
2025-02-05 19:43:04 +0700

    day lai codecode

--- controller/productController.php
+++ controller/productController.php
@@ -1,13 +1,24 @@
 <?php
-require_once '../model/Product.php';
 
-class ProductController{
+require_once 'model/Product.php';
+require_once 'model/Category.php';
+class productController
+{
     public $product;
-    public function __construct(){
+    public $category;
+    public function __construct()
+    {
         $this->product = new Product();
+        $this->category = new Category();
     }
-    public function listProducts(){
-        $products = $this->product->listProduct();
-        require_once '../view/pagines/product/ProductFullWidth.php';
+    public function listProductsController()
+    {
+        $listProducts = $this->product->getProductModel();
+        require_once 'view/pagines/product/listProduct.php';
+    }
+    public function detailProductsController($product_id)
+    {
+        $detailProducts = $this->product->getProductByIdModel($product_id);
+        require_once 'view/pagines/product/ProductDetail.php';
     }
 }
