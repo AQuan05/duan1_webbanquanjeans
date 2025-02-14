fa66f82cb942364306e70e1da352d4a47c0a30a3
toanLa <toananhle2k5@gmail.com>
2025-02-14 23:22:33 +0700

    .

--- controller/PayController.php
+++ controller/PayController.php
@@ -14,13 +14,13 @@ class PayController{
         }
         if(isset($_POST['place_order'])){
 
-        $user = $_SESSION['user'];
+        $user = $_SESSION['user_id'];
         $cart_items = $_SESSION['cart_items'] ?? [];
         $total_amount = array_sum(array_column($cart_items, 'total_price'));
         $shipping_address = $user['user_address'] ?? 'No address';
         $phone = $user['user_phone'] ?? '';
                 // Thêm đơn hàng vào bảng orders
-                $order_id = $this->pay->createOrder($user['id'], $total_amount, $shipping_address, $phone);
+                $order_id = $this->pay->createOrder($user['user_id'], $total_amount, $shipping_address, $phone);
 
                 // Thêm sản phẩm vào bảng order_items
                 $this->pay->addOrderItems($order_id, $cart_items);
