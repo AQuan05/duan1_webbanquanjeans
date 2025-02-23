ec37ef2a9818f0634aac40b09c0eee6e0b963d19
toanLa <toananhle2k5@gmail.com>
2025-02-23 15:15:44 +0700

    sưa lai banner

--- view/pagines/product/home.php
+++ view/pagines/product/home.php
@@ -5,7 +5,10 @@
         <div class="cr-slider swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <div class="cr-hero-banner cr-banner-image-two">
+                    <div class="cr-hero-banner text-center">
+                        <div class="banner-wrapper">
+                            <img src="view/assets/img/banner/banner_8.jpg" alt="" width="100%" height="750px">
+                        </div>
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-12">
@@ -19,7 +22,10 @@
                     </div>
                 </div>
                 <div class="swiper-slide">
-                    <div class="cr-hero-banner cr-banner-image-one">
+                    <div class="cr-hero-banner text-center">
+                        <div class="banner-wrapper">
+                            <img src="view/assets/img/banner/banner_3.jpg" alt="" width="100%" height="750px">
+                        </div>
                         <div class="container">
                             <div class="row">
                                 <div class="col-lg-12">
@@ -36,6 +42,7 @@
             <div class="swiper-pagination"></div>
         </div>
     </section>
+
 
     <!-- Popular product -->
     <section class="section-popular-product-shape padding-b-100">
@@ -93,33 +100,6 @@
         </div>
     </section>
 
-    <!-- Product banner -->
-    <section class="section-product-banner padding-b-100">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="cr-banner-slider swiper-container">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide" data-aos="fade-up" data-aos-duration="2000">
-                                <div class="cr-product-banner-image">
-                                    <img src="view/assets/img/product-banner/1.jpg" alt="product-banner">
-                                </div>
-                            </div>
-                            <div class="swiper-slide" data-aos="fade-up" data-aos-duration="2000">
-                                <div class="cr-product-banner-image">
-                                    <img src="view/assets/img/product-banner/2.jpg" alt="product-banner">
-                                </div>
-                            </div>
-                            <div class="swiper-slide" data-aos="fade-up" data-aos-duration="2000">
-                                <div class="cr-product-banner-image">
-                                    <img src="view/assets/img/product-banner/3.jpg" alt="product-banner">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+
 
     <?php require_once 'view/layout/footer.php' ?>
