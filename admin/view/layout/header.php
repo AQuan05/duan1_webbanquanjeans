03203aaabcad9dc7077f9f79a172b78605dded0d
toanLa <toananhle2k5@gmail.com>
2025-02-24 22:55:36 +0700

    xong phần giao diện  dasboad admin

--- admin/view/layout/header.php
+++ admin/view/layout/header.php
@@ -31,6 +31,8 @@
     <link href="../admin/view/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script> -->
 
 </head>
 
